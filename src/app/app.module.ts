38a7836cab99725f943b0bfa37a619fc75bd3b73
bilmakovchik <ruslanbilmakovskiy@gmail.com>
2020-03-10 22:50:26 +0200

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/modals/profile/profile.component';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MenuComponent } from './components/header/menu/menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
+    MatDialogModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
+  entryComponents: [
+    ProfileComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/modals/profile/profile.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.template.html',
+  styleUrls: ['./profile.style.scss']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ProfileComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
