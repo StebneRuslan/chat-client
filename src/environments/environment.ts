d0d005d2718b3d36de0f81a1be9725ce61cbcc63
yarina2917 <yarina297@gmail.com>
2020-03-11 22:24:40 +0200

    add crypto and interceptor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/'
+  apiUrl: 'http://localhost:3000',
+  secretKey: 'GnsKWV78X88BZQkvqzL2WRu333j10qDI'
 };
 
 /*
